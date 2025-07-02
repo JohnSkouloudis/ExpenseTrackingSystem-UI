88f1b2b08494c40750237072bede4bae1209204c
John skoul <johnskoul56@gmail.com>
2025-07-02 19:38:59 +0300

    added condition to show sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup>
 import Sidebar from './components/Sidebar.vue'
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const showSidebar = computed(() => {
+
+  return !['Login', 'Register','NotFound'].includes(route.name);
+});
 
 </script>
 
 <template>
   
     <div class="flex flex-1  overflow-hidden shadow-lg bg-[#1a1a1a]">
-      <Sidebar />
+      <Sidebar v-if="showSidebar" />
 
       <main class="flex-1 p-6 overflow-y-auto rounded-r-xl">
         <RouterView />
