1a369f140d5dd98abaef45d47e12c8f39707a4e9
John skoul <johnskoul56@gmail.com>
2025-07-02 19:39:56 +0300

    Added views to implement

---
+++ src/views/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen text-white">
+    <h1 class="text-4xl font-bold">404 - Page Not Found</h1>
+    <p class="mt-4 text-lg">The page you're looking for doesn't exist.</p>
+    <RouterLink to="/" class="mt-6 text-green-400 underline">Go back home</RouterLink>
+  </div>
+</template>
