df4a9a402a5ee11ebf9b252d52499c8336ee82da
John skoul <johnskoul56@gmail.com>
2025-07-05 13:24:16 +0300

    changed text of page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -2,6 +2,6 @@
   <div class="flex flex-col items-center justify-center h-screen text-white">
     <h1 class="text-4xl font-bold">404 - Page Not Found</h1>
     <p class="mt-4 text-lg">The page you're looking for doesn't exist.</p>
-    <RouterLink to="/" class="mt-6 text-green-400 underline">Go back home</RouterLink>
+    <RouterLink to="/" class="mt-6 text-green-400 underline">Return home</RouterLink>
   </div>
 </template>
