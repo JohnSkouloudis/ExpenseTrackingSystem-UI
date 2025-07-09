5dcb61c36dbe9976276e831c0ad17abce33f2898
John skoul <johnskoul56@gmail.com>
2025-07-09 12:37:12 +0300

    Added Piechart to Analytics page

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -1,1 +1,31 @@
-<template>analytics</template>
+<template>
+  <div class="flex flex-wrap gap-4 justify-center p-4">
+
+    <ChartBox>
+      <PieChart v-if="categoriesSummary.length" :categoriesSummary="categoriesSummary" />
+      <p v-else class="text-gray-400">Loading data...</p>
+    </ChartBox>
+  </div>
+</template>
+
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import PieChart from '../components/PieChart.vue'
+import { getCategoriesSummary } from '../services/transaction'
+import ChartBox from '../components/ChartBox.vue'
+
+const categoriesSummary = ref([])
+
+
+onMounted(async () => {
+  try {
+    categoriesSummary.value = await getCategoriesSummary()
+  } catch (error) {
+    console.error('Failed to load category summary:', error)
+  }
+})
+
+
+</script>
+
