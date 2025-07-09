626fb1aee52d77532b01e61c826e3731ff8bc73b
John skoul <johnskoul56@gmail.com>
2025-07-09 12:28:53 +0300

    updated user.js

--- src/store/user.js
+++ src/store/user.js
@@ -1,11 +1,13 @@
+
+
 const TOKEN_KEY = 'token';
 
-// Set token
+
 export function setToken(token) {
   localStorage.setItem(TOKEN_KEY, token);
 }
 
-// Get token
+
 export function getToken() {
 
    return localStorage.getItem(TOKEN_KEY);
