677d256d7188ef95fdfbe60755ec36c4d9fb3928
John skoul <johnskoul56@gmail.com>
2025-07-13 16:21:12 +0300

    added functions to get, create,update and delete budget

--- src/services/budget.js
+++ src/services/budget.js
@@ -3,3 +3,15 @@ import api from '../services/api'
 export function getUserBudget() {
   return api.get('/budget/get')
 }
+
+export function updateUserBudget(budget) {
+  return api.put('/budget/update', budget)  
+
+}
+
+export function createUserBudget(budget) {
+  return api.post('/budget/create', budget)
+}
+export function deleteUserBudget() {
+  return api.delete('/budget/delete')
+}
