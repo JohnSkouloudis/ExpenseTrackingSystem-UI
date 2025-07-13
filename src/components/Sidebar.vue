d0f171c9a62b5c53a6206ed3f4c0b7785dfdd64d
John skoul <johnskoul56@gmail.com>
2025-07-13 16:20:19 +0300

    updated sidebar text

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -19,7 +19,7 @@
 
       <!-- Navigation -->
       <nav class="flex flex-col gap-3 mt-6">
-        <RouterLink to="/" class="text-green-500 rounded px-3 py-2 hover:bg-green-900 hover:text-green-300 transition" active-class="bg-green-600 text-white">Dashboard</RouterLink>
+        <RouterLink to="/" class="text-green-500 rounded px-3 py-2 hover:bg-green-900 hover:text-green-300 transition" active-class="bg-green-600 text-white" >Dashboard</RouterLink>
         <RouterLink to="/transactions" class="text-green-500 rounded px-3 py-2 hover:bg-green-900 hover:text-green-300 transition" active-class="bg-green-600 text-white">Transactions</RouterLink>
         <RouterLink to="/accounts" class="text-green-500 rounded px-3 py-2 hover:bg-green-900 hover:text-green-300 transition" active-class="bg-green-600 text-white">Accounts</RouterLink>
         <RouterLink to="/analytics" class="text-green-500 rounded px-3 py-2 hover:bg-green-900 hover:text-green-300 transition" active-class="bg-green-600 text-white">Analytics</RouterLink>
